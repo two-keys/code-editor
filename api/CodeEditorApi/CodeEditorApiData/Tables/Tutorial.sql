3d12d857335b868f710850f3977128af966c13e2
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2021-09-06 20:25:46 -0500

    finished basic data table schema setup

---
+++ api/CodeEditorApi/CodeEditorApiData/Tables/Tutorial.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tutorial]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(100) NULL, 
+    [Author] INT NOT NULL, 
+    [CourseId] INT NULL, 
+    [Prompt] TEXT NULL, 
+    [CreateDate] DATETIME NOT NULL, 
+    [ModifyDate] NCHAR(10) NOT NULL, 
+    [IsPublished] BIT NOT NULL
+)
