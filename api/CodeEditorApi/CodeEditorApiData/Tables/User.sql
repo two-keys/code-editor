f71edfba62805bea6065349f9ef00f8665560edb
CruizK <Cruiz.Kusman@gmail.com>
2021-09-08 14:35:26 -0500

    Updated user table with access token and hash for auth

--- api/CodeEditorApi/CodeEditorApiData/Tables/User.sql
+++ api/CodeEditorApi/CodeEditorApiData/Tables/User.sql
@@ -2,7 +2,9 @@
 (
 	[Id] INT IDENTITY(1,1) PRIMARY KEY,
 	[Name] VARCHAR(100) NOT NULL, 
-	[Email] VARCHAR(255) NOT NULL, 
+	[Email] VARCHAR(255) NOT NULL,
+	[Hash] VARCHAR(255),
+	[AccessToken] VARCHAR(255),
 	[RoleId] INT NOT NULL, 
 	CONSTRAINT [FK_User_cfgRoles] FOREIGN KEY ([RoleId]) REFERENCES [cfgRoles]([Id])
 )
