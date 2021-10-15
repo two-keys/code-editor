157cda0be73882972621a5b395f930ecdbd4b8ac
CruizK <Cruiz.Kusman@gmail.com>
2021-10-15 17:48:24 -0500

    Updated dockerfile again

--- scripts/deployStagingApi.sh
+++ scripts/deployStagingApi.sh
@@ -6,4 +6,4 @@ docker run --rm --name code-editor-db --network="host" code-editor-db
 docker load -i code-editor-api.tar
 docker stop code-editor-api
 docker rm code-editor-api
-docker run -p 8080:80 -d --restart unless-stopped --name code-editor-api --network="host" code-editor-api
+docker run -p 8080:80 -d --restart unless-stopped --name code-editor-api --network="db" code-editor-api
