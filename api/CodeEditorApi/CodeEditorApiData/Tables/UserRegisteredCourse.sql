dccd1c53b1a057e0f997f520086fc7fb1b37ec97
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2021-11-14 16:40:14 -0600

    adding cascade delete to Course Id in UserRegisteredCourse (#64)

--- api/CodeEditorApi/CodeEditorApiData/Tables/UserRegisteredCourse.sql
+++ api/CodeEditorApi/CodeEditorApiData/Tables/UserRegisteredCourse.sql
@@ -3,6 +3,6 @@
 	[CourseId] INT NOT NULL , 
     [UserId] INT NOT NULL, 
     CONSTRAINT [PK_UserRegisteredCourse] PRIMARY KEY ([CourseId], [UserId]), 
-    CONSTRAINT [FK_UserRegisteredCourse_Course] FOREIGN KEY ([CourseId]) REFERENCES [Course]([Id]), 
+    CONSTRAINT [FK_UserRegisteredCourse_Course] FOREIGN KEY ([CourseId]) REFERENCES [Course]([Id]) ON DELETE CASCADE,
     CONSTRAINT [FK_UserRegisteredCourse_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]) ON DELETE CASCADE
 )
