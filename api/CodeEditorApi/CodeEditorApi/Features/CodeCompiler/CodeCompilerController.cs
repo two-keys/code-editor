9c2ca35851aabe4bc0b51e3abb9208d9c5c38a0a
Cruiz <Cruiz.Kusman@gmail.com>
2022-01-21 09:44:50 -0600

    Update CodeCompilerController.cs
    Test for workflow

--- api/CodeEditorApi/CodeEditorApi/Features/CodeCompiler/CodeCompilerController.cs
+++ api/CodeEditorApi/CodeEditorApi/Features/CodeCompiler/CodeCompilerController.cs
@@ -27,6 +27,7 @@ namespace CodeEditorApi.Features.Courses
 
         /// <summary>
         /// Compiles code and returns output
+        /// Adding more to comment for workflow test
         /// </summary>
         /// <returns></returns>
         [HttpGet("Compile")]
