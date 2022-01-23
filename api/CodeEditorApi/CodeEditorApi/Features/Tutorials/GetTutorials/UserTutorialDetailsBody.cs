6b2158d2ab8f5dfbe9f7e20500a9ea13e6ff7db7
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2022-01-23 17:52:49 -0600

    new route called GetUserTutorialDetails - gets both Tutorial info for Course page and User's status per each Tutorial (#91)
    links to User Story #119

---
+++ api/CodeEditorApi/CodeEditorApi/Features/Tutorials/GetTutorials/UserTutorialDetailsBody.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeEditorApi.Features.Tutorials.GetTutorials
+{
+    public class UserTutorialDetailsBody
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int? DifficultyId { get; set; }
+        public int? LanguageId { get; set; }
+        public bool InProgress { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public UserTutorialDetailsBody(int Id, int UserId, int? DifficultyId, int? LanguageId, bool InProgress, bool IsCommpleted)
+        {
+            this.Id = Id;
+            this.UserId = UserId;
+            this.DifficultyId = DifficultyId;
+            this.LanguageId = LanguageId;
+            this.InProgress = InProgress;
+            this.IsCompleted = IsCommpleted;
+        }
+    }
+
+}
---
+++ api/CodeEditorApi/CodeEditorApiUnitTests/Features/Tutorials/GetUserTutorialDetailsCommandTest.cs
@@ -0,0 +1,34 @@
+﻿using AutoFixture;
+using CodeEditorApi.Features.Tutorials.GetTutorials;
+using CodeEditorApiDataAccess.Data;
+using CodeEditorApiUnitTests.Helpers;
+using FluentAssertions;
+using Moq;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeEditorApiUnitTests.Features.Tutorials
+{
+    public class GetUserTutorialDetailsCommandTest : UnitTest<GetUserTutorialsDetailsCommand>
+    {
+        [Fact]
+        public async Task ShouldReturnUserTutorialDetailsBodyList()
+        {
+            var userId = It.IsAny<int>();
+            var courseId = It.IsAny<int>();
+
+            var userTutorialDetails = fixture.Build<UserTutorialDetailsBody>()
+                .With(utdb => utdb.UserId, userId)
+                .CreateMany().ToList();
+
+            Freeze<IGetTutorials>().Setup(gt => gt.GetUserRegisteredTutorialsWithCourseTutorialDetails(courseId, userId)).ReturnsAsync(userTutorialDetails);
+
+            var actionResult = await Target().ExecuteAsync(courseId, userId);
+
+            actionResult.Result.Should().BeNull();
+            actionResult.Value.Should().BeEquivalentTo(userTutorialDetails);
+
+        }
+    }
+}
