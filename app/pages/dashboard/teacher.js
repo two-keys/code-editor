7bb91b719d8248e4853f505ee7458868670a67fa
Cruiz <Cruiz.Kusman@gmail.com>
2022-01-23 18:40:29 -0600

    Update teacher.js

--- app/pages/dashboard/teacher.js
+++ app/pages/dashboard/teacher.js
@@ -27,7 +27,7 @@ export async function getServerSideProps(context) {
             headers: {...headers},
         });
     } catch(e) {
-        console.error(e)
+        console.error(JSON.stringify(e))
     }
     
     if (response && response.statusText == "OK")
