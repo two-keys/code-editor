355d1e665f3fac948d8d6a4077ac0daa59b099fd
CruizK <Cruiz.Kusman@gmail.com>
2022-01-31 20:32:54 -0600

    it will work soon, i swear

--- api/CodeEditorApi/CodeEditorApi/Startup.cs
+++ api/CodeEditorApi/CodeEditorApi/Startup.cs
@@ -4,6 +4,7 @@ using CodeEditorApiDataAccess.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -177,6 +178,11 @@ namespace CodeEditorApi
             }
 
             app.UseHttpsRedirection();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseAuthentication();
             app.UseRouting();
