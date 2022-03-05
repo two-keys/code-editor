4b734109d25136aaa4c625c789968dbe50475850
EmBla312 <43798516+EmBla312@users.noreply.github.com>
2022-03-05 13:08:58 -0600

    Adding IsConfirmed to User table for email confirmations (#129)

--- api/CodeEditorApi/CodeEditorApiData/Tables/User.sql
+++ api/CodeEditorApi/CodeEditorApiData/Tables/User.sql
@@ -6,6 +6,7 @@
 	[Hash] VARCHAR(255),
 	[AccessToken] VARCHAR(255),
 	[RoleId] INT NOT NULL, 
-	CONSTRAINT [FK_User_cfgRoles] FOREIGN KEY ([RoleId]) REFERENCES [cfgRoles]([Id]), 
+	[IsConfirmed] BIT NOT NULL, 
+    CONSTRAINT [FK_User_cfgRoles] FOREIGN KEY ([RoleId]) REFERENCES [cfgRoles]([Id]), 
     CONSTRAINT [AK_Email] UNIQUE ([Email])
 )
